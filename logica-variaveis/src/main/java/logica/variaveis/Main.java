0c47d30d42c5ee758036757b327b8f03bde65123
Carlos Tosin <carlos@softblue.com.br>
2019-06-26 16:14:09 -0300

    Initial commit

---
+++ logica-variaveis/src/main/java/logica/variaveis/Main.java
@@ -0,0 +1,56 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2019 Softblue
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+package logica.variaveis;
+
+@SuppressWarnings("unused")
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//int idade;
+		//idade = 20;
+		
+		int idade = 20;
+		double nota = 8.6;
+		boolean ligado = true;
+		boolean ativo = false;
+		String nome = "Carlos";
+		
+		System.out.println(nota);
+		
+		System.out.println(nome + " -> " + nota);
+		
+		//int 2x;
+		int x2;
+		
+		int numeroDePessoas = 10;
+		
+		var x = 5;
+		var y = "abc";
+		var z = 2.2;
+		var w = true;
+		
+		
+	}
+}
