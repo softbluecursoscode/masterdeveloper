ee854b65100e09aed2a8f7cff37a2c500145bd0f
Carlos Tosin <carlos@softblue.com.br>
2019-07-11 17:23:01 -0300

    Classe de dicionário via arquivo de configuração

--- jogoforca/src/main/java/br/com/softblue/jogoforca/core/Dictionary.java
+++ jogoforca/src/main/java/br/com/softblue/jogoforca/core/Dictionary.java
@@ -1,11 +1,21 @@
 package br.com.softblue.jogoforca.core;
 
+import java.lang.reflect.Constructor;
+
 public abstract class Dictionary {
 	private static Dictionary instance;
 	
 	public static Dictionary getInstance() {
 		if (instance == null) {
-			instance = new StaticDictionary();
+			try {
+				String dictionaryClassName = Config.get("dictionaryClassName");
+				Class<?> clazz = Class.forName(dictionaryClassName);
+				Constructor<?> constructor = clazz.getConstructor();
+				instance = (Dictionary) constructor.newInstance();
+				
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
 		}
 		return instance;
 	}
