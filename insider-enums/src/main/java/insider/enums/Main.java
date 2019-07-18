4d177cf824d235b8e138ac4a8e971303e2a2928c
Carlos Tosin <carlos@softblue.com.br>
2019-07-18 16:03:34 -0300

    Initial commit

---
+++ insider-enums/src/main/java/insider/enums/Main.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2019 Softblue
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+package insider.enums;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		DiaDaSemana d1 = DiaDaSemana.Quinta;
+		System.out.println(d1.isDiaUtil());
+		
+		DiaDaSemana d2 = DiaDaSemana.Sabado;
+		System.out.println(d2.isDiaUtil());
+		
+		DiaDaSemana domingo = DiaDaSemana.valueOf("Domingo");
+		System.out.println(domingo);
+		
+		System.out.println(DiaDaSemana.valueOf("Sexta").isDiaUtil());
+	}
+}
