c4d1428993aa0d4a5e93a9120f254ee4ef7b00b2
Carlos Tosin <carlos@softblue.com.br>
2019-10-29 12:41:25 -0300

    Copyright

--- bluefood/bin/main/data.sql
+++ bluefood/bin/main/data.sql
@@ -1,4 +1,4 @@
-###############################################################################
+/*###############################################################################
 # MIT License
 #
 # Copyright (c) 2019 Softblue
@@ -20,7 +20,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-###############################################################################
+###############################################################################*/
 
 INSERT INTO categoria_restaurante (nome, imagem) VALUES ('Pizza', '0001-categoria.png');
 INSERT INTO categoria_restaurante (nome, imagem) VALUES ('Sanduíche', '0002-categoria.png');
