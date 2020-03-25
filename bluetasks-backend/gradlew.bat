bf0a99e4cd0e41edce4d3c0cc791a11273aeb684
Carlos <Carlos@DESKTOP-JQ0BEN5>
2020-03-25 22:10:02 +0000

    Project fix

--- bluetasks-backend/gradlew.bat
+++ bluetasks-backend/gradlew.bat
@@ -1,3 +1,27 @@
+@rem ***************************************************************************
+@rem
+@rem MIT License
+@rem
+@rem Copyright (c) 2020 Softblue
+@rem
+@rem Permission is hereby granted, free of charge, to any person obtaining a copy
+@rem of this software and associated documentation files (the "Software"), to deal
+@rem in the Software without restriction, including without limitation the rights
+@rem to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+@rem copies of the Software, and to permit persons to whom the Software is
+@rem furnished to do so, subject to the following conditions:
+@rem
+@rem The above copyright notice and this permission notice shall be included in all
+@rem copies or substantial portions of the Software.
+@rem
+@rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+@rem IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+@rem FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+@rem AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+@rem LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+@rem OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+@rem SOFTWARE.
+@rem ***************************************************************************
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
